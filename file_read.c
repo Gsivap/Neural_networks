0e63881c7af51dd302936c02e794365864747995
Sivaramakrishnan Sankarapandian <scelesticsiva@gmail.com>
2017-04-16 00:01:25 -0400

    updated file read sample C file

--- file_read.c
+++ file_read.c
@@ -5,10 +5,17 @@ int main()
     FILE *fp;
     float temp;
     char buff[100];
+    int rows = 100;
+    int columns = 3;
+    float x_train[rows][columns];
     fp = fopen("x_train.txt","r");
-    for(int i=0;i<300;i++)
+    for(int i = 0;i<rows;i++)
     {
-        fscanf(fp,"%f",&temp);
-        printf("%f",temp);
+        for(int j = 0;j<columns;j++)
+        {
+            //if(fscanf(fp,"%f",&temp)>0)
+            fscanf(fp,"%f",&x_train[i][j]);
+            printf("%f----%d\n",x_train[i][j],(j+3*i));
+        }
     }
 }
